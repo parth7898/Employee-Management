3c6c7c157b3ad56e93bb9fe9fac8c2a0ff3baf13
parth <parthnew3@gmail.com>
2024-04-25 14:43:36 +0530

    Add routes for employees

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :employees, only: [:create]
+  get '/employees/tax_deduction', to: 'employees#tax_deduction'
 end
